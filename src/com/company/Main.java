b08994f8aa97d27f05396091130acc1d5648f11b
madskolsen <89842092+madskolsen@users.noreply.github.com>
2022-02-21 10:07:06 +0100

    Første gren

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello from Branch1");
-
+        System.out.println("A leaf on the branch");
     }
 }
